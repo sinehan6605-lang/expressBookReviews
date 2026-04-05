84f852a5d87847e6478a2306479d8caea164bd46
Sinehan <sinehan6605@gmail.com>
2026-04-05 11:46:00 +0530

    added general.js with axios

--- test.js
+++ test.js
@@ -6,15 +6,15 @@ const {
 } = require('./general');
 
 (async () => {
-//   console.log("===== TASK 10: Get All Books =====");
-//   console.log(await getAllBooks());
+  console.log("===== TASK 10: Get All Books =====");
+  console.log(await getAllBooks());
 
-//   console.log("\n===== TASK 11: Search by ISBN =====");
-//   console.log(await getBookByISBN(1));
+  console.log("\n===== TASK 11: Search by ISBN =====");
+  console.log(await getBookByISBN(1));
 
-//   console.log("\n===== TASK 12: Search by Author =====");
-//   console.log(await getBooksByAuthor("Paulo Coelho"));
+  console.log("\n===== TASK 12: Search by Author =====");
+  console.log(await getBooksByAuthor("Paulo Coelho"));
 
-  console.log("\n===== TASK 13: Search by Title =====");
-  console.log(await getBooksByTitle("Atomic Habits"));
+   console.log("\n===== TASK 13: Search by Title =====");
+   console.log(await getBooksByTitle("Atomic Habits"));
 })();
